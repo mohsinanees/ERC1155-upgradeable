431f13e891aa744fc757433f0c3a5e30362991fd
Rakhi <rakhi.soni.gsr@gmail.com>
2021-09-29 23:43:28 +0530

    withdraw and recovery added

--- contracts/PDOGStaking.sol
+++ contracts/PDOGStaking.sol
@@ -3,6 +3,66 @@
 pragma solidity ^0.8.0;
 
 
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+
 interface IERC20 {
 
     function totalSupply() external view returns (uint256);
@@ -354,7 +414,7 @@ contract Pausable is Ownable {
 }
 
 
-contract PDOGStaking is Ownable, Pausable {		
+contract PDOGStaking is Ownable, Pausable, ReentrancyGuard {		
     using SafeMath for uint256;
 	string public name = "PDOG - Staking";
     IERC20 public tokenA;
@@ -367,9 +427,21 @@ contract PDOGStaking is Ownable, Pausable {
 	mapping(address => bool) public hasStaked;
 	mapping(address => bool) public isStaking;
 	mapping(address => uint256) public oldReward;
+	uint256 private _totalSupply;
 	
 	event Reward(address indexed from, address indexed to, uint256 amount);
 	event StakeTransfer(address indexed from, address indexed to, uint256 amount);
+	event Withdrawn(address indexed user, uint256 amount);
+	
+    event ExternalTokenTransfered(
+        address from,
+        address to,
+        uint256 amount
+    );
+    event EthFromContractTransferred(
+        uint256 amount
+    );
+    
 
 	constructor(IERC20 _tokenA, uint256 _rewardRate, uint256 _blockLimit) {
 		tokenA = _tokenA;
@@ -382,7 +454,7 @@ contract PDOGStaking is Ownable, Pausable {
 	to starting earning rewards.
 		
 	Core Thing: Transfer the tokenA from the investor's wallet to this smart contract. */
-	function stakeTokenA(uint _amount) public whenNotPaused {
+	function stakeTokenA(uint _amount) public nonReentrant whenNotPaused {
         require(block.number >= blockLimit, "current block number is below the Block Limit");		
         require(_amount > 0, "staking balance cannot be 0");
         require(tokenA.balanceOf(msg.sender) > _amount);
@@ -407,12 +479,14 @@ contract PDOGStaking is Ownable, Pausable {
 		
     }
     
-    function unstakeTokenA() public whenNotPaused {
+    function unstakeTokenA() public nonReentrant whenNotPaused {
         require(isStaking[msg.sender], "User have no staked tokens to unstake");
         uint balance = stakedBalance[msg.sender];
         require(balance > 0, "staking balance cannot be 0");
+        require(tokenA.balanceOf(address(this)) > balance, "Not Enough staked tokens in the smart contract");
         uint256 reward = calculateReward();
         uint256 totalReward = reward.add(oldReward[msg.sender]);
+        require(rewardToken.balanceOf(address(this)) > totalReward, "Not Enough tokens in the smart contract");
         
         rewardToken.transfer(msg.sender, totalReward);
         emit Reward(address(this), msg.sender, totalReward);
@@ -439,8 +513,19 @@ contract PDOGStaking is Ownable, Pausable {
 
     }
     
+     function withdrawFromStaked(uint256 amount) public nonReentrant {
+        require(amount > 0, "Cannot withdraw 0");
+        stakedBalance[msg.sender] = stakedBalance[msg.sender].sub(amount);
+        tokenA.transfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function getTotalStaked() public view returns (uint256) {
+        return tokenA.balanceOf(address(this));
+    }
+    
    
-    function claimMyReward() public whenNotPaused {
+    function claimMyReward() public nonReentrant whenNotPaused {
         require(isStaking[msg.sender], "User have no staked tokens to get the reward");
         uint balance = stakedBalance[msg.sender];
         require(balance > 0, "staking balance cannot be 0");
@@ -453,6 +538,28 @@ contract PDOGStaking is Ownable, Pausable {
 		stakingStartTime[msg.sender] = block.timestamp;
 		
 	}
+	
+	function withdrawERC20Token(address _tokenContract, uint256 _amount) external onlyOwner {
+        require(_tokenContract != address(0), "Address cant be zero address");
+		// require amount greter than 0
+		require(_amount > 0, "amount cannot be 0");
+        IERC20 tokenContract = IERC20(_tokenContract);
+        require(tokenContract.balanceOf(address(this)) > _amount);
+		tokenContract.transfer(msg.sender, _amount);
+        emit ExternalTokenTransfered(_tokenContract, msg.sender, _amount);
+	}
+	
+	function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+	
+	function withdrawETHFromContract(uint256 amount) public onlyOwner {
+        require(amount <= getBalance());        
+        address payable _owner = payable(msg.sender);        
+        _owner.transfer(amount);        
+        emit EthFromContractTransferred(amount);
+    }
+    
 
     function setRewardRate(uint256 _rewardRate) external onlyOwner whenNotPaused {
         rewardRate = _rewardRate;
