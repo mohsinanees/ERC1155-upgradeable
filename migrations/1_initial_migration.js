fb3f49ad9411b710e88d7d24d9c33ae006687507
Gowtham Paramasivam <gowthamparamasivam66@gmail.com>
2021-10-28 08:46:02 +0530

    contract upgradeable proxy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
 const PDOGStaking = artifacts.require("PDOGStaking");
 const PDOG = artifacts.require("PDOG");
 const Web3 = require('web3');
-const {deployProxy} = require("@openzeppelin/truffle-upgrades");
+const {deployProxy, upgradeProxy} = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer, network) {
 
@@ -13,7 +13,8 @@ module.exports = async function (deployer, network) {
     /**
      * params - _stakeToken, _rewardToken, _rewardRateInWei, _startTime, _rewardIntervalInSeconds
      */
-    await deployer.deploy(PDOGStaking, pdog.address, pdog.address, (20*(10**18)).toString(), currentTime, 60);
+    // await deployer.deploy(PDOGStaking, pdog.address, pdog.address, (20*(10**18)).toString(), currentTime, 60);
+    await deployProxy(PDOGStaking, [pdog.address, pdog.address, (20*(10**18)).toString(), currentTime, 60], { deployer, initializer: 'initialize' });
   }
   if(network === "testnet") {
     var web3 =  new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
@@ -28,13 +29,13 @@ module.exports = async function (deployer, network) {
   if(network === "testnet1") {
     var web3 =  new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
     await deployer.deploy(PDOG);
-    let pdog = await PDOG.deployed();
+    // let pdog = await PDOG.deployed();
     const currentTime = Math.floor(new Date().getTime() / 1000);
 
     /**
      * params - _stakeToken, _rewardToken, _rewardRateInWei, _startTime, _rewardIntervalInSeconds
      */
-    await deployProxy(PDOGStaking, [pdog.address, pdog.address, (20*(10**18)).toString(), currentTime, 60], { deployer, initializer: 'initialize' });
-    // let pdogStaking = await PDOGStaking.deployed();
+    // await deployProxy(PDOGStaking, [pdog.address, pdog.address, (20*(10**18)).toString(), currentTime, 60], { deployer, initializer: 'initialize' });
+    const upgraded = await upgradeProxy('0x3645D927D0Aa7b2bB2114e864c175Dbed814B5F5', PDOGStaking, { deployer });
   }
 };
